80145023ceb6d723f250bea2d1c9b9db1bb47bfa
minaalbertsaeed <mina.albert33@gmail.com>
2024-11-02 21:35:44 +0200

    Add exercise4.c

--- chapter5/exercise4.c
+++ chapter5/exercise4.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
 
 int mydup (int oldfd){
     int newfd = fcntl(oldfd, F_DUPFD);
@@ -14,8 +15,33 @@ int mydup (int oldfd){
     return newfd;
 }
 
+int mydup2 (int oldfd, int newfd){
+    if (oldfd == newfd){
+        int test = fcntl(oldfd, F_GETFL);
+        if (test == -1 ){
+            errno = EBADF;
+            return -1;
+        }
+        return newfd;
+    }
+
+
+    int res = fcntl(oldfd, F_DUPFD, newfd);
+
+    if (res == -1){
+        printf("ERROR duplicating %d\n", oldfd);
+        return EXIT_FAILURE;
+    }
+
+    return res;
+}
 int main(int argc, char *argv[])
 {
-    mydup(0);
+    const char *file = "./test_files/1.txt";
+    int fd = open(file, O_CREAT);
+    /* case 1 */
+    // mydup2(fd, fd );
+    /* case 2 */
+    // mydup2(5, 5 );
     return 0;
 }
