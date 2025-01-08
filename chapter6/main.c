8e2cdb4b8a9f3e1fa0c66b913c8f8d09b2f53439
minaalbertsaeed <mina.albert33@gmail.com>
2025-01-09 01:08:22 +0200

    Add chapter6/main.c

---
+++ chapter6/main.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+extern char **environ;
+//...
+
+int main(){
+
+    int i = 0;
+
+    while(environ[i]) {
+        printf("%s\n", environ[i++]); // prints in form of "variable=value"
+    }
+}
+
