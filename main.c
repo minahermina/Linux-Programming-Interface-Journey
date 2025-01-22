f0582fdfc4b07afe708d81feeb6c762cc41d3171
minaalbertsaeed <mina.albert33@gmail.com>
2025-01-22 11:37:55 +0200

    Edit main.c

--- main.c
+++ main.c
@@ -1,27 +1,27 @@
-#define _FILE_OFFSET_BITS 64
-#include<stdio.h>
-#include<unistd.h>
-int
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int 
 main(int argc, char *argv[])
 {
-#define  MAX_LEN  10
- 
-   FILE *stream;
-   char buffer[MAX_LEN + 1];
-   int  result;
-   int  i;
-   char ch;
- 
-   stream = fopen("test.txt", "r");
-   for (i = 0; (i  < (sizeof(buffer)-1) &&
-       ((ch = fgetc(stream)) != EOF) && (ch != '\n')); i++)
-          buffer[i] = ch;
- 
-   result = fseek(stream, 0L, SEEK_SET);  /* moves the pointer to the */
-                                          /* beginning of the file    */
-   if (result == 0)
-      printf("Pointer successfully moved to the beginning of the file.\n");
-   else
-      printf("Failed moving pointer to the beginning of the file.\n");
-}
+    /* int i = 0;
+    while(environ[i]){
+        printf("%s\n", environ[i]);
+        i++;
+    } */
 
+    int x = 1;
+    void* ptr;
+    while(x--){
+        ptr = malloc(3*sizeof(char));
+        printf("x: %d\n", x);
+        free(ptr);
+    }
+    printf("%p\n", ptr);
+    return 0;
+
+}
